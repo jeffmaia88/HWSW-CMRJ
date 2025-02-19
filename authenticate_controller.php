2a568039ec667501088e0846acf28b17cc0bfd33
jeff.maia88 <jefferson.reis88@gmail.com>
2025-02-19 19:25:19 -0300

    criação de controller de autenticação de login

---
+++ authenticate_controller.php
@@ -0,0 +1,29 @@
+<?php
+
+$usuario_autenticado = false;
+
+$usuario_app = array (
+    array('login' => 'jefferson.maia', 'senha' => 'dezembro88'),
+    array('login' => 'mauro.neves', 'senha' => '123456'),
+);
+
+
+
+foreach($usuario_app as $user) {
+    
+    if($user['login'] == strtolower($_POST['login']) && $user['senha'] == $_POST['password']) {
+        $usuario_autenticado = true;
+    }
+}
+
+
+if ($usuario_autenticado) {
+    echo 'Usuario autenticado';
+}else {
+    header('location: index.php?login=error');
+}
+
+
+
+
+?>
